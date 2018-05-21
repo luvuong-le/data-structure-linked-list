79357fb4494938feb80e323b574b3de02d8041ce
luvuong-le <lu-vuongle@hotmail.com>
2018-05-21 22:53:11 +1000

    Update package.json and added files

---
+++ dist/app.js
@@ -0,0 +1,245 @@
+"use strict";
+exports.__esModule = true;
+var s_linked_list_1 = require("./s-linked-list");
+var d_linked_list_1 = require("./d-linked-list");
+var chalk = require('chalk');
+var prompt = require('inquirer').prompt;
+var art = require('ascii-art');
+var readline = require('readline');
+/* Set up readline module interface
+   Creates a new readline.Interface instance
+*/
+var readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+readlineInterface.on("close", function () {
+    console.log("Exiting Program...");
+});
+var sll = new s_linked_list_1.SLL();
+var dll = new d_linked_list_1.DLL();
+function start() {
+    // Render ASCII Art title
+    art.font('Linked lists', 'Doom', 'red+bold', function (rendered) {
+        console.log(rendered);
+        init();
+    });
+}
+function action(time, callback) {
+    setTimeout(function () {
+        clearScreen();
+        callback();
+    }, time);
+}
+function clearScreen() {
+    process.stdout.write("\x1B[2J\x1B[0f");
+}
+function showMain() {
+    console.log(chalk.red("\nData Structures: Linked Lists"));
+    console.log("------------------------");
+    console.log("1: Singly Linked List \n2: Doubly Linked List \n3: Exit Program");
+    readlineInterface.question(chalk.blue('\nPick an option > '), function (answer) {
+        // Switch Statement Here
+        switch (answer.trim()) {
+            case '1':
+                action(1000, function () {
+                    showSLLMenu();
+                });
+                break;
+            case '2':
+                action(1000, function () { showDLLMenu(); });
+                break;
+            case '3':
+                action(1000, function () { readlineInterface.close(); process.exit(); });
+                break;
+            default:
+                console.log(chalk.red('\n[ERROR] Please pick a valid option!'));
+                action(1000, start);
+                break;
+        }
+        ;
+    });
+}
+function showSLLMenu() {
+    // art.font('Singly', 'Doom', 'red+bold', (rendered) => {
+    //   console.log(rendered);
+    // });
+    console.log(chalk.red("\nData Structures: Singly Linked Lists"));
+    console.log("------------------------");
+    console.log("1: Get List Size \n2: Prepend \n3: Append \n4: Remove First \n5: Remove Last \n6: Remove By Number \n7: Remove at Position \n8: Get At Specific Position \n9: Print List \n10: Back to Main Menu");
+    readlineInterface.question(chalk.blue('\nPick an option > '), function (answer) {
+        switch (answer.trim()) {
+            case "1":
+                action(1000, function () {
+                    console.log("List length: " + sll.length);
+                    showSLLMenu();
+                });
+                break;
+            case "2":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an number to prepend > "), function (answer) {
+                        sll.prepend(parseInt(answer));
+                        showSLLMenu();
+                    });
+                });
+                break;
+            case "3":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an number to append > "), function (answer) {
+                        sll.append(parseInt(answer));
+                        showSLLMenu();
+                    });
+                });
+                break;
+            case "4":
+                action(1000, function () {
+                    sll.removeFirst();
+                    showSLLMenu();
+                });
+                break;
+            case "5":
+                action(1000, function () {
+                    sll.removeLast();
+                    showSLLMenu();
+                });
+                break;
+            case "6":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an number > "), function (answer) {
+                        sll.remove(parseInt(answer));
+                        showSLLMenu();
+                    });
+                });
+                break;
+            case "7":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an position > "), function (answer) {
+                        sll.removeAtPosition(parseInt(answer));
+                        showSLLMenu();
+                    });
+                });
+                break;
+            case "8":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an position > "), function (answer) {
+                        sll.getAtPosition(parseInt(answer));
+                        showSLLMenu();
+                    });
+                });
+                break;
+            case "9":
+                action(1000, function () {
+                    sll.print();
+                    showSLLMenu();
+                });
+                break;
+            case "10":
+                action(1000, function () {
+                    start();
+                });
+                break;
+            default:
+                console.log(chalk.red("\n[ERROR] Please enter a valid option"));
+                action(1000, function () { return showSLLMenu(); });
+                break;
+        }
+        ;
+    });
+}
+function showDLLMenu() {
+    // art.font('Doubly', 'Doom', 'red+bold', (rendered) => {
+    //   console.log(rendered);
+    // });
+    console.log(chalk.red("\nData Structures: Doubly Linked Lists"));
+    console.log("------------------------");
+    console.log("1: Get List Size \n2: Prepend \n3: Append \n4: Remove First \n5: Remove Last \n6: Remove By Number \n7: Remove at Position \n8: Get At Specific Position \n9: Print List Forwards \n10: Print List Backwards \n11: Back to Main Menu");
+    readlineInterface.question(chalk.blue('\nPick an option > '), function (answer) {
+        switch (answer.trim()) {
+            case "1":
+                action(1000, function () {
+                    console.log("List length: " + dll.length);
+                    showDLLMenu();
+                });
+                break;
+            case "2":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an number to prepend > "), function (answer) {
+                        dll.prepend(parseInt(answer));
+                        showDLLMenu();
+                    });
+                });
+                break;
+            case "3":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an number to append > "), function (answer) {
+                        dll.append(parseInt(answer));
+                        showDLLMenu();
+                    });
+                });
+                break;
+            case "4":
+                action(1000, function () {
+                    dll.removeFirst();
+                    showDLLMenu();
+                });
+                break;
+            case "5":
+                action(1000, function () {
+                    dll.removeLast();
+                    showDLLMenu();
+                });
+                break;
+            case "6":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an number > "), function (answer) {
+                        dll.remove(parseInt(answer));
+                        showDLLMenu();
+                    });
+                });
+                break;
+            case "7":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an position > "), function (answer) {
+                        dll.removeAtPosition(parseInt(answer));
+                        showDLLMenu();
+                    });
+                });
+                break;
+            case "8":
+                action(1000, function () {
+                    readlineInterface.question(chalk.blue("\nEnter an position > "), function (answer) {
+                        dll.getAtPosition(parseInt(answer));
+                        showDLLMenu();
+                    });
+                });
+                break;
+            case "9":
+                action(1000, function () {
+                    dll.print();
+                    showDLLMenu();
+                });
+                break;
+            case "10":
+                action(1000, function () {
+                    dll.printReverse();
+                    showDLLMenu();
+                });
+                break;
+            case "11":
+                action(1000, function () {
+                    start();
+                });
+                break;
+            default:
+                console.log(chalk.red("\n[ERROR] Please enter a valid option"));
+                action(1000, function () { return showDLLMenu(); });
+                break;
+        }
+        ;
+    });
+}
+function init() {
+    showMain();
+}
+// Start the app by showing the title
+start();
---
+++ dist/d-linked-list.js
@@ -0,0 +1,305 @@
+"use strict";
+exports.__esModule = true;
+var Node_1 = require("./Node");
+var util = require('util');
+var chalk = require("chalk");
+var DLL = /** @class */ (function () {
+    function DLL() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    /**
+     * @function [isEmpty]
+     * @description Check if linked list length is 0 or not
+     * @returns boolean
+     */
+    DLL.prototype.isEmpty = function () {
+        return this.length === 0 ? true : false;
+    };
+    /**
+     * @function [getSize]
+     * @description Return the size of the list
+     * @returns number
+     */
+    DLL.prototype.getSize = function () {
+        return this.length;
+    };
+    DLL.prototype.log = function (message, color) {
+        console.log(color(message));
+    };
+    /**
+     * @function [contains]
+     * @argument value
+     * @description Check if doubly linked list contains a value
+     * @returns boolean
+     */
+    DLL.prototype.contains = function (value) {
+        var current = this.head;
+        while (current !== null) {
+            if (current.value === value) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    };
+    /**
+     * @function [prepend]
+     * @description: Append a new node to beginning of list
+     */
+    DLL.prototype.prepend = function (value) {
+        var node = new Node_1.Node(value, this.head, null);
+        if (this.head !== null) {
+            // Set the previous of the current head to the new node
+            this.head.prev = node;
+            // Set the head equal to the new node
+            this.head = node;
+        }
+        else {
+            this.head = node;
+            this.tail = node;
+        }
+        this.length++;
+        this.log("\n[SUCCESS] Node with value [" + value + "] prepended successfully", chalk.green);
+    };
+    /**
+     *  @function [append]
+     *  @description: Create node and append to end of list
+     **/
+    DLL.prototype.append = function (value) {
+        var node = new Node_1.Node(value);
+        if (this.tail !== null) {
+            this.tail.next = node;
+        }
+        this.tail = node;
+        if (this.head === null) {
+            this.head = node;
+        }
+        else {
+            var current = this.head;
+            var prev = void 0;
+            while (current.next !== null) {
+                prev = current;
+                current = current.next;
+            }
+            node.prev = prev;
+        }
+        this.log("\n[SUCCESS] Node with value [" + value + "] appended successfully", chalk.green);
+        this.length++;
+    };
+    /**
+     *  @function [removeFirst]
+     *  @description: Remove the current node and set to the nextNode                   in the list
+     *  @return boolean
+     **/
+    DLL.prototype.removeFirst = function () {
+        if (this.getSize() > 0) {
+            if (this.getSize() === 1) {
+                this.head = null;
+                this.tail = null;
+            }
+            else {
+                var current = this.head;
+                var nextNode = current.next;
+                current.next.prev = null;
+                current = null;
+                this.head = nextNode;
+            }
+            this.length--;
+            this.log("\n[SUCCESS] First Item Removed Successfully", chalk.green);
+        }
+        return true;
+    };
+    /**
+     *  @function [removeFirst]
+     *  @description: Remove the last node in the list
+     **/
+    DLL.prototype.removeLast = function () {
+        if (this.getSize() > 0) {
+            var current = this.head;
+            var prev = void 0;
+            while (current.next !== null) {
+                prev = current;
+                current = current.next;
+            }
+            this.tail = this.tail.prev;
+            if (this.getSize() === 1) {
+                this.head = null;
+            }
+            else {
+                prev.next = null;
+                current = null;
+            }
+            this.length--;
+            this.log("\n[SUCCESS] Last Item Removed Successfully", chalk.green);
+        }
+    };
+    /**
+     *  @function [remove]
+     *  @argument number
+     *  @description: Remove a node with a specific number
+     **/
+    DLL.prototype.remove = function (value) {
+        var current = this.head;
+        var prev;
+        if (this.contains(value)) {
+            // If the current head value is equal to the value to remove
+            if (current.value === value) {
+                prev = current.next;
+                current = null;
+                this.head = prev;
+                if (this.head !== null) {
+                    this.head.prev = null;
+                }
+                this.length--;
+                return true;
+            }
+            else {
+                while (current !== null) {
+                    if (current.value === value) {
+                        if (current.next !== null) {
+                            prev.next = current.next;
+                            current.next.prev = prev;
+                            current.next = null;
+                            this.length--;
+                        }
+                        else {
+                            prev.next = null;
+                            current.next = null;
+                            this.tail = prev;
+                            this.length--;
+                        }
+                    }
+                    prev = current;
+                    current = current.next;
+                }
+                this.log("\n[SUCCESS] [" + value + "] Removed Successfully", chalk.green);
+                return true;
+            }
+        }
+        this.log("\n[ERROR] No Such Value", chalk.red);
+        return false;
+    };
+    /**
+     *  @function [removeAtPosition]
+     *  @argument positionIndex
+     *  @description: Remove a node at a specific index
+     **/
+    DLL.prototype.removeAtPosition = function (pos) {
+        if (pos <= this.getSize() && pos > 0) {
+            var current = this.head;
+            var currentIndex = 1;
+            var prev = void 0;
+            if (pos === 1) {
+                prev = current.next;
+                current = null;
+                this.head = prev;
+                if (this.head !== null) {
+                    this.head.prev = null;
+                }
+                this.length--;
+                return true;
+            }
+            else {
+                while (currentIndex <= pos) {
+                    if (currentIndex === this.getSize()) {
+                        prev.next = null;
+                        this.tail = prev;
+                        current = null;
+                        break;
+                    }
+                    if (currentIndex === pos) {
+                        prev.next = current.next;
+                        current.next.prev = prev;
+                        current = null;
+                        break;
+                    }
+                    prev = current;
+                    current = current.next;
+                    currentIndex++;
+                }
+                this.length--;
+                this.log("\n[SUCCESS]: Node at Position " + pos + " removed \n", chalk.green);
+                return true;
+            }
+        }
+        this.log("\n[ERROR][CODE=REMOVAL]: No node at position " + pos + "\n", chalk.red);
+        return false;
+    };
+    /**
+     *  @function [getAtPosition]
+     *  @argument positionIndex
+     *  @description: Get the value of a node at a specific index
+     *  @returns number
+     **/
+    DLL.prototype.getAtPosition = function (pos) {
+        var current = this.head;
+        var currentIndex = 1;
+        if (pos <= this.getSize() && pos !== 0) {
+            while (currentIndex++ !== pos) {
+                current = current.next;
+            }
+            this.log("\n[SUCCESS] Item at Position " + pos + ": " + current.value, chalk.green);
+            return current.value;
+        }
+        this.log("\n[ERROR][CODE=GET]: No node at position " + pos + "\n", chalk.red);
+    };
+    /**
+     *  @function [print]
+     *  @description: Prints the list in order
+     **/
+    DLL.prototype.print = function () {
+        this.log("\nDisplaying Current List Forwards", chalk.blue);
+        this.log("-----------------------------", chalk.blue);
+        if (this.isEmpty()) {
+            this.log("List is Empty", chalk.blue);
+        }
+        var current = this.head;
+        while (current !== null) {
+            if (current.next === null) {
+                process.stdout.write("[" + current.value + "]\n\n");
+            }
+            else {
+                process.stdout.write("[" + current.value + "] => ");
+            }
+            current = current.next;
+        }
+    };
+    /**
+     *  @function [printReverse]
+     *  @description: Prints the list in reverse order
+     **/
+    DLL.prototype.printReverse = function () {
+        this.log("\nDisplaying Current List Backwards", chalk.blue);
+        this.log("-----------------------------", chalk.blue);
+        if (this.isEmpty()) {
+            this.log("List is Empty", chalk.blue);
+        }
+        var current = this.tail;
+        while (current !== null) {
+            if (current.prev === null) {
+                process.stdout.write("[" + current.value + "]\n\n");
+            }
+            else {
+                process.stdout.write("[" + current.value + "] => ");
+            }
+            current = current.prev;
+        }
+    };
+    return DLL;
+}());
+exports.DLL = DLL;
+// const linkedlist = new DLL();
+// linkedlist.append(3);
+// linkedlist.append(4);
+// // linkedlist.append(4);
+// linkedlist.remove(3);
+// linkedlist.remove(4);
+// linkedlist.print();
+// linkedlist.printReverse();
+// // linkedlist.remove(10);
+// // linkedlist.remove(40);
+// linkedlist.removeAtPosition(4);
+// linkedlist.print();
+// linkedlist.printReverse();
